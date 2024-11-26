2634dd36d4f5c405aec49d02942cd5ad34973226
None083 <perezgonzaleznoelia08@gmail.com>
2024-11-26 10:02:46 +0100

    halp

---
+++ REACT/mi-primera-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Saluda extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+}
+}
+
+ReactDOM.render(<Saluda />, document.getElementById('root'));
