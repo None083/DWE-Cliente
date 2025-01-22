4f2ff9b21abc7fd341dd6ae24cf895551eda7e34
None083 <perezgonzaleznoelia08@gmail.com>
2025-01-22 10:06:09 +0100

    test hooks listo, class empezado

--- REACT/RA3/hooks/test-enganchaera/src/App.js
+++ REACT/RA3/hooks/test-enganchaera/src/App.js
@@ -4,37 +4,67 @@ import { Form, Label, Input, FormGroup } from 'reactstrap';
 
 function App() {
   const preguntasIniciales = [
-    "Cuando mandas un mensaje por WhatsApp esperas siempre al doble check. Si no aparece vuelves a abrir el terminal para revisarlo al rato.",
-    "Antes de acostarte siempre miras el móvil a ver si tienes mensajes o notificaciones.",
-    "Te despiertas antes de tiempo para jugar, mandar mensajes, actualizar perfiles,… con el teléfono móvil.",
-    "Si sales de casa sin el móvil volverías a cogerlo aunque llegues tarde a tu cita.",
-    "Tienes miedo a quedarte sin batería.",
-    "Cuando tienes la batería baja desactivas ciertas aplicaciones u opciones del teléfono como la WiFi, bluetooth para no quedarte sin batería.",
-    "Tienes ansiedad cuando tienes llamadas perdidas. Llamas a los números y te preocupas si no responden.",
-    "Miras la cobertura cuando estás en algún sitio con los amigos, esperando, etc.",
-    "Sueles hacer alguna otra cosa mientras que miras al móvil como comer, lavarte los dientes, etc.",
-    "Vas al baño siempre con el móvil.",
+    "1. Cuando mandas un mensaje por WhatsApp esperas siempre al doble check. Si no aparece vuelves a abrir el terminal para revisarlo al rato.",
+    "2. Antes de acostarte siempre miras el móvil a ver si tienes mensajes o notificaciones.",
+    "3. Te despiertas antes de tiempo para jugar, mandar mensajes, actualizar perfiles,… con el teléfono móvil.",
+    "4. Si sales de casa sin el móvil volverías a cogerlo aunque llegues tarde a tu cita.",
+    "5. Tienes miedo a quedarte sin batería.",
+    "6. Cuando tienes la batería baja desactivas ciertas aplicaciones u opciones del teléfono como la WiFi, bluetooth para no quedarte sin batería.",
+    "7. Tienes ansiedad cuando tienes llamadas perdidas. Llamas a los números y te preocupas si no responden.",
+    "8. Miras la cobertura cuando estás en algún sitio con los amigos, esperando, etc.",
+    "9. Sueles hacer alguna otra cosa mientras que miras al móvil como comer, lavarte los dientes, etc.",
+    "10. Vas al baño siempre con el móvil.",
   ];
 
-  // Estado para las preguntas restantes y la cantidad de respuestas "Sí".
   const [preguntas, setPreguntas] = useState(preguntasIniciales);
   const [respuestasSi, setRespuestasSi] = useState(0);
 
-  // Maneja cuando el usuario responde una pregunta
   const handleRespuesta = (respuesta, index) => {
     if (respuesta === 'si') {
-      setRespuestasSi(respuestasSi + 1); // Incrementa el contador de "Sí"
+      setRespuestasSi(respuestasSi + 1);
     }
-    // Elimina la pregunta actual del listado
     const nuevasPreguntas = preguntas.filter((_, i) => i !== index);
     setPreguntas(nuevasPreguntas);
   };
 
+  function calcularResultado() {
+    let resultado = "";
+
+    if (respuestasSi <= 4) {
+      resultado = <div className="alert alert-success mt-4">
+        <h4>Resultado:</h4>
+        <p>Tu puntuación es de {respuestasSi}.</p>
+        <p>En principio no tienes nada de que preocuparte.</p>
+      </div>;
+    } else if (respuestasSi <= 6) {
+      resultado = <div className="alert alert-primary mt-4">
+        <h4>Resultado:</h4>
+        <p>Tu puntuación es de {respuestasSi}.</p>
+        <p>Empiezas a tener signos de dependencia del móvil. Puedes utilizar técnicas como apagar el móvil cuando no lo necesitas, cuando duermes, etc.</p>
+      </div>;
+    } else if (respuestasSi <= 8) {
+      resultado = <div className="alert alert-warning mt-4">
+        <h4>Resultado:</h4>
+        <p>Tu puntuación es de {respuestasSi}.</p>
+        <p>Tienes una gran dependencia del móvil. Deberías seguir un plan de «desintoxicación» del móvil como por ejemplo dejar el móvil en casa cuando vas a comprar, apagarlo durante la noche, apagarlo durante horas de clase o trabajo, etc.</p>
+      </div>;
+    } else {
+      resultado = <div className="alert alert-danger mt-4">
+        <h4>Resultado:</h4>
+        <p>Tu puntuación es de {respuestasSi}.</p>
+        <p>Tus síntomas de dependencia son muy preocupantes. Además de todas las técnicas de los apartados anteriores deberías plantearte un plan de desintoxicación del móvil que consista en estar una o dos semanas sin utilizarlo. Si ves que no puedes hacerlo por ti mismo, pide ayuda a un profesional. </p>
+      </div>;
+    }
+
+    return resultado;
+  }
+
   return (
     <div className="App p-4">
+      <h1>Test nomofobia</h1>
       <Form>
         {preguntas.map((pregunta, index) => (
-          <FormGroup 
+          <FormGroup
             tag="fieldset"
             key={index}
             className="p-3 mb-3 border rounded bg-light"
@@ -46,7 +76,7 @@ function App() {
               <Input
                 type="radio"
                 name={`pregunta-${index}`}
-                onClick={() => handleRespuesta('si', index)} // Maneja el clic en "Sí"
+                onClick={() => handleRespuesta('si', index)}
                 style={{ marginRight: '5px' }}
               />
               <Label check style={{ marginBottom: '0' }}>
@@ -57,7 +87,7 @@ function App() {
               <Input
                 type="radio"
                 name={`pregunta-${index}`}
-                onClick={() => handleRespuesta('no', index)} // Maneja el clic en "No"
+                onClick={() => handleRespuesta('no', index)}
                 style={{ marginRight: '5px' }}
               />
               <Label check style={{ marginBottom: '0' }}>
@@ -67,13 +97,9 @@ function App() {
           </FormGroup>
         ))}
       </Form>
-      {/* Muestra el número de respuestas "Sí" cuando terminan las preguntas */}
-      {preguntas.length === 0 && (
-        <div className="alert alert-success mt-4">
-          <h4>Test terminado</h4>
-          <p>Respondiste "Sí" a {respuestasSi} pregunta(s).</p>
-        </div>
-      )}
+
+      {preguntas.length === 0 && (calcularResultado())}
+
     </div>
   );
 }
--- REACT/RA3/hooks/test-enganchaera2/src/App.js
+++ REACT/RA3/hooks/test-enganchaera2/src/App.js
@@ -1,91 +1,33 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 
-function App() {
-  const preguntas = [
-    "Cuando mandas un mensaje por WhatsApp esperas siempre al doble check. Si no aparece vuelves a abrir el terminal para revisarlo al rato.",
-    "Antes de acostarte siempre miras el móvil a ver si tienes mensajes o notificaciones.",
-    "Te despiertas antes de tiempo para jugar, mandar mensajes, actualizar perfiles,… con el teléfono móvil.",
-    "Si sales de casa sin el móvil volverías a cogerlo aunque llegues tarde a tu cita.",
-    "Tienes miedo a quedarte sin batería.",
-    "Cuando tienes la batería baja desactivas ciertas aplicaciones u opciones del teléfono como la WiFi, bluetooth para no quedarte sin batería.",
-    "Tienes ansiedad cuando tienes llamadas perdidas. Llamas a los números y te preocupas si no responden.",
-    "Miras la cobertura cuando estás en algún sitio con los amigos, esperando, etc.",
-    "Sueles hacer alguna otra cosa mientras que miras al móvil como comer, lavarte los dientes, etc.",
-    "Vas al baño siempre con el móvil.",
-  ];
-
-  const [respuestas, setRespuestas] = useState(Array(preguntas.length).fill(false));
-  const [resultado, setResultado] = useState("");
-
-  const handleCambios = (index, value) => {
-    const newRespuestas = [...respuestas];
-    newRespuestas[index] = value;
-    setRespuestas(newRespuestas);
-  };
-
-  const calculaResultado = () => {
-    const respuestasAfirmativas = respuestas.filter((r) => r).length;
-
-    if (respuestasAfirmativas <= 5) {
-      setResultado("En principio no tienes nada de qué preocuparte.");
-    } else if (respuestasAfirmativas <= 6) {
-      setResultado(
-        "Empiezas a tener signos de dependencia del móvil. Puedes utilizar técnicas como apagar el móvil cuando no lo necesitas, cuando duermes, etc."
-      );
-    } else if (respuestasAfirmativas <= 8) {
-      setResultado(
-        "Tienes una gran dependencia del móvil. Deberías seguir un plan de «desintoxicación» del móvil como por ejemplo dejar el móvil en casa cuando vas a comprar, apagarlo durante la noche, apagarlo durante horas de clase o trabajo, etc."
-      );
-    } else {
-      setResultado(
-        "Tus síntomas de dependencia son muy preocupantes. Además de todas las técnicas de los apartados anteriores deberías plantearte un plan de desintoxicación del móvil que consista en estar una o dos semanas sin utilizarlo. Si ves que no puedes hacerlo por ti mismo, pide ayuda a un profesional."
-      );
+class App extends Component() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      preguntas: [
+        "1. Cuando mandas un mensaje por WhatsApp esperas siempre al doble check. Si no aparece vuelves a abrir el terminal para revisarlo al rato.",
+        "2. Antes de acostarte siempre miras el móvil a ver si tienes mensajes o notificaciones.",
+        "3. Te despiertas antes de tiempo para jugar, mandar mensajes, actualizar perfiles,… con el teléfono móvil.",
+        "4. Si sales de casa sin el móvil volverías a cogerlo aunque llegues tarde a tu cita.",
+        "5. Tienes miedo a quedarte sin batería.",
+        "6. Cuando tienes la batería baja desactivas ciertas aplicaciones u opciones del teléfono como la WiFi, bluetooth para no quedarte sin batería.",
+        "7. Tienes ansiedad cuando tienes llamadas perdidas. Llamas a los números y te preocupas si no responden.",
+        "8. Miras la cobertura cuando estás en algún sitio con los amigos, esperando, etc.",
+        "9. Sueles hacer alguna otra cosa mientras que miras al móvil como comer, lavarte los dientes, etc.",
+        "10. Vas al baño siempre con el móvil.",
+      ],
+      respuestasSi: 0
     }
-  };
+  }
 
-  return (
-    <div className="App">
-      <div className="container mt-5">
-        <h1>Test de Dependencia del Móvil</h1>
-        <Form>
-          {preguntas.map((p, index) => (
-            <FormGroup key={index} className="my-3">
-              <Label>{p}</Label>
-              <div>
-                <Input
-                  type="radio"
-                  name={`p-${index}`}
-                  onChange={() => handleCambios(index, true)}
-                  checked={respuestas[index] === true}
-                />{" "}
-                Sí
-                <Input
-                  type="radio"
-                  name={`p-${index}`}
-                  onChange={() => handleCambios(index, false)}
-                  checked={respuestas[index] === false}
-                  className="ms-3"
-                />{" "}
-                No
-              </div>
-            </FormGroup>
-          ))}
-          <Button color="primary" onClick={calculaResultado} className="mt-4">
-            Calcular Resultado
-          </Button>
-        </Form>
-        {resultado && (
-          <div className="mt-4 alert alert-info">
-            <h4>Resultado:</h4>
-            <p>{resultado}</p>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+  render() {
+    return (
+      <></>
+    )
+  }
 }
 
 export default App;
